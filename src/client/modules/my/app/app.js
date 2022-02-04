b9860a66d0f14b6b026ad5d2c97977a4ff407c71
naveen kothuri <kothurinaveen.kn@gmail.com>
2022-02-04 10:10:51 +0530

    Added the modal to edit the record

--- src/client/modules/my/app/app.js
+++ src/client/modules/my/app/app.js
@@ -1,14 +1,34 @@
 import { LightningElement, wire } from 'lwc';
 // eslint-disable-next-line @lwc/lwc/no-unknown-wire-adapters
 import getAccountRecords from 'data/sfWire';
+const actions = [
+    { label: 'Edit', name: 'edit' },
+    { label: 'Delete', name: 'delete' }
+];
 const columns = [
     { label: 'SolarBot Status Name', fieldName: 'Name' },
     { label: 'SolarBot Name', fieldName: 'SolarBotName' },
-    { label: 'Account Name', fieldName: 'SolarBotAccountName' }
+    { label: 'Account Name', fieldName: 'SolarBotAccountName' },
+    {
+        type: 'action',
+        typeAttributes: { rowActions: actions, menuAlignment: 'left' }
+    }
 ];
 export default class App extends LightningElement {
-    result;
+    result = {
+        Id: 'a002w000003Mz7WAAS',
+        Name: 'Naveen',
+        SolarBot__r: {
+            Id: 'a002w000003Mz7WAAS',
+            Name: 'Kothuri',
+            Account__r: {
+                Id: 'a002w000003Mz7WAAS',
+                Name: 'Naveen Kothuri'
+            }
+        }
+    };
     columns = columns;
+    editRecord;
     // eslint-disable-next-line @lwc/lwc/no-unknown-wire-adapters
     @wire(getAccountRecords) getRecords({ data, error }) {
         console.log('Fetching the records');
@@ -24,5 +44,30 @@ export default class App extends LightningElement {
         if (error) {
             console.log('got error', error);
         }
+    }
+    handleRowAction(event) {
+        const action = event.detail.action;
+        const row = event.detail.row;
+        // eslint-disable-next-line default-case
+        switch (action.name) {
+            case 'edit':
+                this.editRecord = row;
+                break;
+            case 'delete':
+                // eslint-disable-next-line no-case-declarations
+                const rows = this.result;
+                // eslint-disable-next-line no-case-declarations
+                const rowIndex = rows.indexOf(row);
+                rows.splice(rowIndex, 1);
+                this.data = rows;
+                break;
+        }
+    }
+
+    handleCancel() {
+        this.editRecord = undefined;
+    }
+    handleSave() {
+        console.log(this.editRecord);
     }
 }
